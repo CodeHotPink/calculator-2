6c991841c693a5a1a7d41c7bb5fa11cc995955de
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-18 20:15:54 -0700

    add addition function

--- calculator.py
+++ calculator.py
@@ -13,3 +13,9 @@ Get input from user
 	check conditions to know which math function to run
 	call the math function
 """
+
+user_input = input("")
+math_list = user_input.split(" ")
+if math_list[0] == "+":
+	print(add(float(math_list[1]),float(math_list[2])))
+	
