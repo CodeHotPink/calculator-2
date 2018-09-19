3db7dd27a768442b4d363799cef05f5e7c76f7ef
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-18 20:07:03 -0700

    add pseudocode

---
+++ calculator.py
@@ -0,0 +1,15 @@
+"""A prefix-notation calculator.
+
+Using the arithmetic.py file from Calculator Part 1, create the
+calculator program yourself in this file.
+"""
+
+from arithmetic import *
+
+
+"""
+Keep looping until "q"
+Get input from user
+	check conditions to know which math function to run
+	call the math function
+"""
